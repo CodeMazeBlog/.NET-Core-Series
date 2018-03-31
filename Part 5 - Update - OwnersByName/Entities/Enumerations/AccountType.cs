518b53328a665eabc7bab1a57bcb0da427d5ce1f
Marinko <marinkospasojevic@gmail.com>
2018-03-31 10:10:34 +0200

    Updated part 5 - OwnersByName

---
+++ Part 5 - Update - OwnersByName/Entities/Enumerations/AccountType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Enumerations
+{
+    public enum AccountType
+    {
+        Domestic,
+        Savings,
+        Foreign
+    }
+}
---
+++ Part 5 - Update - OwnersByName/Repository/OwnerRepository.cs
@@ -0,0 +1,46 @@
+﻿using Contracts;
+using Entities;
+using Entities.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Entities.ExtendedModels;
+
+namespace Repository
+{
+    public class OwnerRepository: RepositoryBase<Owner>, IOwnerRepository
+    {
+        public OwnerRepository(RepositoryContext repositoryContext)
+            :base(repositoryContext)
+        {
+        }
+
+        public IEnumerable<Owner> GetAllOwners()
+        {
+            return FindAll()
+                .OrderBy(ow => ow.Name);
+        }
+
+        public Owner GetOwnerById(Guid ownerId)
+        {
+            return FindByCondition(owner => owner.Id.Equals(ownerId))
+                    .DefaultIfEmpty(new Owner())
+                    .FirstOrDefault();
+        }
+
+        public OwnerExtended GetOwnerWithDetails(Guid ownerId)
+        {
+            return new OwnerExtended(GetOwnerById(ownerId))
+            {
+                Accounts = RepositoryContext.Accounts
+                    .Where(a => a.OwnerId == ownerId)
+            };
+        }
+
+
+        public IEnumerable<Owner> GetOwnersByName(string name)
+        {
+            return FindByCondition(ow => ow.Name.Equals(name));
+        }
+    }
+}
