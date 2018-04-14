b4de9f6d169061b45249a2bfa41526c508ef827a
Marinko <marinkospasojevic@gmail.com>
2018-04-14 18:13:09 +0200

    Modified RepositoryWrapper.

--- Part 5/Contracts/IRepositoryWrapper.cs
+++ Part 5/Contracts/IRepositoryWrapper.cs
@@ -6,7 +6,7 @@ namespace Contracts
 {
     public interface IRepositoryWrapper
     {
-        IOwnerRepository Owner { get; set; }
-        IAccountRepository Account { get; set; }
+        IOwnerRepository Owner { get; }
+        IAccountRepository Account { get; }
     }
 }
--- Part 5/Repository/RepositoryWrapper.cs
+++ Part 5/Repository/RepositoryWrapper.cs
@@ -5,13 +5,39 @@ namespace Repository
 {
     public class RepositoryWrapper : IRepositoryWrapper
     {
-        public IOwnerRepository Owner { get; set; }
-        public IAccountRepository Account { get; set; }
+        private RepositoryContext _repoContext;
+        private IOwnerRepository _owner;
+        private IAccountRepository _account;
+
+        public IOwnerRepository Owner
+        {
+            get
+            {
+                if (_owner == null)
+                {
+                    _owner = new OwnerRepository(_repoContext);
+                }
+
+                return _owner;
+            }
+        }
+
+        public IAccountRepository Account
+        {
+            get
+            {
+                if (_account == null)
+                {
+                    _account = new AccountRepository(_repoContext);
+                }
+
+                return _account;
+            }
+        }
 
         public RepositoryWrapper(RepositoryContext repositoryContext)
         {
-            this.Owner = new OwnerRepository(repositoryContext);
-            this.Account = new AccountRepository(repositoryContext);
+            _repoContext = repositoryContext;
         }
     }
 }
