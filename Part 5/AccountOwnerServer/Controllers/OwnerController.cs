3e0ef1ca93bae0d86c4508705def9c2a9d446c21
Marinko <marinkospasojevic@gmail.com>
2019-02-17 22:41:36 +0100

    .net core version 2.2 support

---
+++ Part 5/AccountOwnerServer/Controllers/OwnerController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccountOwnerServer.Controllers
+{
+    [Route("api/owner")]
+    [ApiController]
+    public class OwnerController : ControllerBase
+    {
+        private ILoggerManager _logger;
+        private IRepositoryWrapper _repository;
+
+        public OwnerController(ILoggerManager logger, IRepositoryWrapper repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOwners()
+        {
+            try
+            {
+                var owners = _repository.Owner.GetAllOwners();
+
+                _logger.LogInfo($"Returned all owners from database.");
+
+                return Ok(owners);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetAllOwners action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetOwnerById(Guid id)
+        {
+            try
+            {
+                var owner = _repository.Owner.GetOwnerById(id);
+
+                if (owner.Id.Equals(Guid.Empty))
+                {
+                    _logger.LogError($"Owner with id: {id}, hasn't been found in db.");
+                    return NotFound();
+                }
+                else
+                {
+                    _logger.LogInfo($"Returned owner with id: {id}");
+                    return Ok(owner);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetOwnerById action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpGet("{id}/account")]
+        public IActionResult GetOwnerWithDetails(Guid id)
+        {
+            try
+            {
+                var owner = _repository.Owner.GetOwnerWithDetails(id);
+
+                if (owner.Id.Equals(Guid.Empty))
+                {
+                    _logger.LogError($"Owner with id: {id}, hasn't been found in db.");
+                    return NotFound();
+                }
+                else
+                {
+                    _logger.LogInfo($"Returned owner with details for id: {id}");
+                    return Ok(owner);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong inside GetOwnerWithDetails action: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
---
+++ Part 5/Contracts/IOwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities.ExtendedModels;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Contracts
+{
+    public interface IOwnerRepository:IRepositoryBase<Owner>
+    {
+        IEnumerable<Owner> GetAllOwners();
+        Owner GetOwnerById(Guid ownerId);
+        OwnerExtended GetOwnerWithDetails(Guid ownerId);
+    }
+}
