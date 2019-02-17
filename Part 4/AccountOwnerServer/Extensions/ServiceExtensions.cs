acf0456112cb5c3f4d1995b15a47473ffda57ab6
Marinko <marinkospasojevic@gmail.com>
2019-02-17 20:30:16 +0100

    New version 2.2 for .NET Core

---
+++ Part 4/AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Contracts;
+using Entities;
+using LoggerService;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Repository;
+
+namespace AccountOwnerServer.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options => 
+            {
+                
+            });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+
+        public static void ConfigureMySqlContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config["mysqlconnection:connectionString"];
+            services.AddDbContext<RepositoryContext>(o => o.UseMySql(connectionString));
+        }
+
+        public static void ConfigureRepositoryWrapper(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+        }
+    }
+}
---
+++ Part 4/Contracts/IOwnerRepository.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Contracts
+{
+    public interface IOwnerRepository:IRepositoryBase<Owner>
+    {
+    }
+}
