95e6a277cdfd9ca820beb5a474e3b2c364ca2b40
Marinko <marinkospasojevic@gmail.com>
2020-07-15 08:18:23 +0200

    fixed environment import address

--- Part 9/src/app/shared/services/environment-url.service.ts
+++ Part 9/src/app/shared/services/environment-url.service.ts
@@ -1,4 +1,4 @@
-import { environment } from './../../../environments/environment.prod';
+import { environment } from './../../../environments/environment';
 import { Injectable } from '@angular/core';
 
 @Injectable({
