cec275da1a1be89f0719ceebc7eab0bbd8ee531a
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-20 11:41:09 +0200

    Updated to Angular 13

--- Part 8/src/app/app.module.ts
+++ Part 8/src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'
- 
+import { BrowserModule } from '@angular/platform-browser';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { NotFoundComponent } from './error-pages/not-found/not-found.component';
- 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,12 +18,9 @@ import { NotFoundComponent } from './error-pages/not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([
-      { path: 'home', component: HomeComponent },
-      { path: '404', component : NotFoundComponent},
-      { path: '', redirectTo: '/home', pathMatch: 'full' },
-      { path: '**', redirectTo: '/404', pathMatch: 'full'}
-    ])
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
