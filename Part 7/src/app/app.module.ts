da7b80e2708091de54b85daf0db7adcc15354d22
MarinkoSpasojevic <marinkospasojevic@gmail.com>
2022-04-20 11:40:16 +0200

    Updated to Angular 13

--- Part 7/src/app/app.module.ts
+++ Part 7/src/app/app.module.ts
@@ -1,7 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
@@ -10,7 +12,9 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
