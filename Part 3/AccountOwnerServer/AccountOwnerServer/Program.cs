929da1e94d6112b8496b1c0b2a3b95961d88014e
Code Maze <contact@code-maze.com>
2025-01-22 12:38:54 +0100

    Updated to .NET 9

--- Part 3/AccountOwnerServer/AccountOwnerServer/Program.cs
+++ Part 3/AccountOwnerServer/AccountOwnerServer/Program.cs
@@ -4,7 +4,7 @@ using NLog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+LogManager.Setup().LoadConfigurationFromFile(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
 
 builder.Services.ConfigureCors(); 
 builder.Services.ConfigureIISIntegration();
